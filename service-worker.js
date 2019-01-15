b760c1c98f4568d66e5bd017a973d5f42668567d
Patrick Stockwell <stockwellpatrick@gmail.com>
2019-01-15 18:17:45 +1100

    add fetch event handler

--- service-worker.js
+++ service-worker.js
@@ -2,6 +2,7 @@ console.log('inside the service worker');
 console.log(self);
 
 var CACHE_NAME = 'to-do-site-cache-v1';
+var CACHE_WHITELIST = [ CACHE_NAME ];
 var urlsToCache = [
   '/',
   'style.css',
@@ -19,3 +20,30 @@ self.addEventListener('install', function(event) {
       })
   );
 });
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.forEach(function(cacheName) {
+          if (!CACHE_WHITELIST.includes(cacheName)) {
+            caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(cachedItem) {
+        if (cachedItem) {
+          return cachedItem;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
